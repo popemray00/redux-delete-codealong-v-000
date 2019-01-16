cfeb3ed4639d3e2011d75126dda9ba8e455e865b
Michael Pope <popemray1986@gmail.com>
2019-01-16 17:14:09 +0000

    Automatically backed up by Learn

--- src/reducers/manageTodo.js
+++ src/reducers/manageTodo.js
@@ -8,7 +8,7 @@ export default function manageTodo(state = {
        id: Math.random()*10000000000000000,
        text: action.payload.text
      }
-      
+
       return { todos: state.todos.concat(action.payload.text) };
 
       case 'DELETE_TODO':
